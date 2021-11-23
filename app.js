61b7d74a10aeae51da1fbdda9a8f132b77f785dd
spaghettimedic <dsteiner247@gmail.com>
2021-11-22 20:13:01 -0500

    closed 'capture user input'

---
+++ app.js
@@ -0,0 +1,24 @@
+const profileDataArgs = process.argv.slice(2, process.argv.length);
+
+// Notice the lack of parentheses around the 'profileDataArr' parameter?
+const printProfileData = profileDataArr => {
+    // This...
+    for (let i = 0; i < profileDataArr.length; i++) {
+        console.log(profileDataArr[i]);
+    }
+
+    console.log("================");
+
+    // Is the same as this...
+    profileDataArr.forEach((profileItem) => {
+        console.log(profileItem)
+    });
+
+    console.log("================");
+
+    // And this...
+    profileDataArr.forEach(profileItem => console.log(profileItem));
+};
+
+
+printProfileData(profileDataArgs);
