658b848870a8e4f341ddbdec39b1e4e83d0eb876
spaghettimedic <dsteiner247@gmail.com>
2021-11-23 09:32:43 -0500

    built HTML template and generated HTML file

--- app.js
+++ app.js
@@ -1,30 +1,18 @@
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
+
 const profileDataArgs = process.argv.slice(2, process.argv.length);
 // This...
 // const name = profileDataArgs[0];
 // const github = profileDataArgs[1];
 // Is this the same as this...
 const [name, github] = profileDataArgs;
-const generatePage = (userName, githubName) => {
-    return `
-    <!DOCTYPE html>
-    <html lang="en">
-        <head>
-            <meta charset="UTF-8">
-            <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            <meta http-equiv="X-UA-Compatible" content="ie=edge">
-            <title>Portfolio Demo</title>
-        </head>
 
-        <body>
-            <h1>${name}</h1>
-            <h2><a href="https://github.com/${github}">Github</a></h2>
-        </body>
-    </html>
-    `;
-};
+fs.writeFile('index.html', generatePage(name, github), err => {
+    if (err) throw err;
 
-console.log(name, github);
-console.log(generatePage(name, github));
+    console.log('Portfolio complete! Check out index.html to see the output!');
+});
 
 
 // // Notice the lack of parentheses around the 'profileDataArr' parameter?
