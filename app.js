b154a86ea23378b34f77c089819c9f48aa94aa7a
spaghettimedic <dsteiner247@gmail.com>
2021-11-25 08:11:55 -0500

    installed inquirer and wrote first prompt, added gitignore put node_modules in it

--- app.js
+++ app.js
@@ -1,39 +1,21 @@
-const fs = require('fs');
-const generatePage = require('./src/page-template.js');
+const inquirer = require('inquirer');
+// const fs = require('fs');
+// const generatePage = require('./src/page-template.js');
 
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-// This...
-// const name = profileDataArgs[0];
-// const github = profileDataArgs[1];
-// Is this the same as this...
-const [name, github] = profileDataArgs;
+// const pageHTML = generatePage(name, github);
 
-fs.writeFile('index.html', generatePage(name, github), err => {
-    if (err) throw err;
+// fs.writeFile('index.html', generatePage(name, github), err => {
+//     if (err) throw err;
 
-    console.log('Portfolio complete! Check out index.html to see the output!');
-});
+//     console.log('Portfolio complete! Check out index.html to see the output!');
+// });
 
-
-// // Notice the lack of parentheses around the 'profileDataArr' parameter?
-// const printProfileData = profileDataArr => {
-//     // This...
-//     for (let i = 0; i < profileDataArr.length; i++) {
-//         console.log(profileDataArr[i]);
-//     }
-
-//     console.log("================");
-
-//     // Is the same as this...
-//     profileDataArr.forEach((profileItem) => {
-//         console.log(profileItem)
-//     });
-
-//     console.log("================");
-
-//     // And this...
-//     profileDataArr.forEach(profileItem => console.log(profileItem));
-// };
-
-
-// printProfileData(profileDataArgs);
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is your name?'
+        }
+    ])
+    .then(answers => console.log(answers));
