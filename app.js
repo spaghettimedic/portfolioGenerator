a25c0b2ac47d1191db3492f7e678a8d488abd5ee
spaghettimedic <dsteiner247@gmail.com>
2021-11-25 16:58:15 -0500

    created project section HTML

--- app.js
+++ app.js
@@ -1,14 +1,6 @@
 const inquirer = require('inquirer');
-// const fs = require('fs');
-// const generatePage = require('./src/page-template.js');
-
-// const pageHTML = generatePage(name, github);
-
-// fs.writeFile('index.html', generatePage(name, github), err => {
-//     if (err) throw err;
-
-//     console.log('Portfolio complete! Check out index.html to see the output!');
-// });
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
 
 const promptUser = () => {
     return inquirer.prompt([
@@ -142,5 +134,11 @@ const promptProject = portfolioData => {
 promptUser()
     .then(promptProject)
     .then(portfolioData => {
-        console.log(portfolioData);
+        const pageHTML = generatePage(portfolioData);
+        
+        fs.writeFile('index.html', pageHTML, err => {
+            if (err) throw new Error(err);
+        
+            console.log('Page created! Check out index.html in this directory to see it!');
+        });
     });
